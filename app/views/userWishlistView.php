6a79453d33712785c481f2db24853f2fd92954a0
jeremy hennebique <jimimi@hotmail.fr>
2018-10-26 10:19:41 +0200

    conditionnement wishlist si celle ci est vide

--- app/views/userWishlistView.php
+++ app/views/userWishlistView.php
@@ -1,33 +1,42 @@
 <?php ob_start(); ?>
 <div id="userListArea" class="col-lg-10">
-
-<table id="wishlist" class="table table-responsive-xl table-dark">
-  <tbody>
-    <?php
-      while($item = $wishlist->fetch()){
-echo '<tr class="item' . $item['itemId'] . '">'; ?>
-       <td><img class='itemImg'/></td>
-       <td class="itemId"><?php echo $item['itemId']; ?></td>
-       <td class="itemName"></td>
-       <td>Supprimer</td>
-    </tr>
-        <?php
-          }
-        ?>
-  </tbody>
-</table>
-  <!--Area appear hover IMG-->
-    <div id="itemDetailList">
-      <h4 id="itemName"></h4>
-      <ul id=itemStats>
-        <li id="itemLvl"></li>
-        <li id="bind"></li>
-        <li id="armor"></li>
-        <!--Stats Bonus area-->
-        <li id="durability"></li>
-        <li id="itemLvlRequiered"></li>
-      </ul>
-    </div>
+<?php
+if($wishlist->fetch() === false){
+  ?>
+  <p>Votre liste de souhaits est vide pour le moment, ajoutez y des objets après avoir utilisé la fonction de recherche.</p>
+  <?php
+}else{
+  ?>
+    <table id="wishlist" class="table table-responsive-xl table-dark">
+    <tbody>
+      <?php
+        while($item = $wishlist->fetch()){
+  echo '<tr class="item' . $item['itemId'] . '">'; ?>
+         <td><img class='itemImg'/></td>
+         <td class="itemId"><?php echo $item['itemId']; ?></td>
+         <td class="itemName"></td>
+         <td>Supprimer</td>
+      </tr>
+          <?php
+            }
+          ?>
+    </tbody>
+  </table>
+<?php
+  }
+?>
+<!--Area appear hover IMG-->
+<div id="itemDetailList">
+  <h4 id="itemName"></h4>
+    <ul id=itemStats>
+      <li id="itemLvl"></li>
+      <li id="bind"></li>
+      <li id="armor"></li>
+      <!--Stats Bonus area-->
+      <li id="durability"></li>
+      <li id="itemLvlRequiered"></li>
+    </ul>
+</div>
 
 <script>
 
@@ -39,8 +48,7 @@ $(document).ready(function(){
   })
 
 });
-
-
 </script>
+
 <?php $content = ob_get_clean(); ?>
 <?php require('template/template.php'); ?>
