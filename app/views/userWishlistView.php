35689f2169460ad0dd9ed652bb494408b65d1d57
jeremy hennebique <jimimi@hotmail.fr>
2018-10-26 11:11:52 +0200

    ajout unique d'item en wishlist

--- app/views/userWishlistView.php
+++ app/views/userWishlistView.php
@@ -1,31 +1,20 @@
 <?php ob_start(); ?>
 <div id="userListArea" class="col-lg-10">
-<?php
-if($wishlist->fetch() === true){
-  ?>
-  <p>Votre liste de souhaits est vide pour le moment, ajoutez y des objets après avoir utilisé la fonction de recherche.</p>
-  <?php
-}else{
-  ?>
-    <table id="wishlist" class="table table-responsive-xl table-dark">
-    <tbody>
-      <?php
-      //var_dump($wishlist->fetch());
-        while($item = $wishlist->fetch()){
-  echo '<tr class="item' . $item['itemId'] . '">'; ?>
-         <td><img class='itemImg'/></td>
-         <td class="itemId"><?php echo $item['itemId']; ?></td>
-         <td class="itemName"></td>
-         <td>Supprimer</td>
-      </tr>
-          <?php
-            }
-          ?>
-    </tbody>
-  </table>
-<?php
-  }
-?>
+  <table id="wishlist" class="table table-responsive-xl table-dark">
+  <tbody>
+    <?php
+      while($item = $wishlist->fetch()){
+echo '<tr class="item' . $item['itemId'] . '">'; ?>
+       <td><img class='itemImg'/></td>
+       <td class="itemId"><?php echo $item['itemId']; ?></td>
+       <td class="itemName"></td>
+       <td>Supprimer</td>
+    </tr>
+        <?php
+          }
+        ?>
+  </tbody>
+</table>
 <!--Area appear hover IMG-->
 <div id="itemDetailList">
   <h4 id="itemName"></h4>
